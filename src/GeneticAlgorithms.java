71d808e1f3cbd3f1b0d43c95ff6d2052b3b1b475
ercaany <ercaany@gmail.com>
2016-12-24 04:52:14 +0300

    crossover duzeltilecek

--- src/GeneticAlgorithms.java
+++ src/GeneticAlgorithms.java
@@ -40,4 +40,41 @@ public class GeneticAlgorithms {
         childChromosome.mutation(pixels);
         return childChromosome;
     }
+
+    public Chromosome crossoverForCircle(Chromosome parent1, Chromosome parent2) {
+
+        int width = parent1.getImg().width();
+        int height = parent1.getImg().height();
+        int type = parent1.getImg().type();
+        int edgeSize = parent1.getEdgeCoordinates().size();
+
+        Chromosome childChromosome = new Chromosome();
+        Mat child = new Mat(width, height, type);
+        List<Coordinate> edges = new ArrayList<>();
+
+        List<Integer> template = new ArrayList<>();
+
+        Random random = new Random();
+        for(int i = 0; i < edgeSize; i++) {
+            template.add(random.nextInt(2));
+        }
+
+        Coordinate coordinate;
+        for(int i = 0; i < edgeSize; i++) {
+            if(template.get(i) == 0) {
+                coordinate = parent1.getEdgeCoordinates().get(i);
+                edges.add(coordinate);
+                child.put(coordinate.getX(), coordinate.getY(), 255.0);
+            } else {
+                coordinate = parent2.getEdgeCoordinates().get(i);
+                edges.add(coordinate);
+                child.put(coordinate.getX(), coordinate.getY(), 255.0);
+            }
+        }
+
+        childChromosome.setImg(child);
+        childChromosome.setEdgeCoordinates(edges);
+        //childChromosome.mutationForCircle();
+        return childChromosome;
+    }
 }
